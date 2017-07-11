7915acc8cba9517b1e7b9b1c0edb5d6ab64ef7d2
Victor Mataré <matare@fh-aachen.de>
2017-07-11 14:57:43 +0200

    add rcll-simulate-field.bash

---
+++ bin/rcll-simulate-field.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MACHINES=(BS CS1 CS2 RS1 RS2 DS)
+TEAMS=(C)
+
+if [[ -n $TMUX ]]; then
+	for t in $TEAMS; do
+		for m in $MACHINES; do
+			tmux new-window bash -c "./rcll-simulate-mps -m ${t}-${m} || read"
+		done
+	done
+else
+	echo "Must be started in tmux! Exiting."
+	exit 1
+fi
