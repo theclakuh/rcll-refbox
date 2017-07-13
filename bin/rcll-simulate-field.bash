3a661c0343091aec7d4aabeb1c0675a656b5a865
Tobias Neumann <t.neumann@fh-aachen.de>
2017-07-13 15:07:58 +0200

    rcll-simulate-field.bash: add screen as an option

--- bin/rcll-simulate-field.bash
+++ bin/rcll-simulate-field.bash
@@ -2,14 +2,23 @@
 
 MACHINES=(BS CS1 CS2 RS1 RS2 DS)
 TEAMS=(C)
+SCREEN=false
+if [[ "$TERM" == screen.* ]] ; then
+  SCREEN=true
+fi
 
-if [[ -n $TMUX ]]; then
+if [[ -n $TMUX ]] || [[ $SCREEN ]]; then
 	for t in ${TEAMS[@]}; do
 		for m in ${MACHINES[@]}; do
-			tmux new-window bash -c "./rcll-simulate-mps -m ${t}-${m}; read"
+      if [[ -n $TMUX ]] ; then
+			  tmux new-window bash -c "./rcll-simulate-mps -m ${t}-${m}; read"
+      fi
+      if [[ $SCREEN ]]; then
+        screen bash -c "./rcll-simulate-mps -m ${t}-${m}; read"
+      fi
 		done
 	done
 else
-	echo "Must be started in tmux! Exiting."
+	echo "Must be started in tmux or screen! Exiting."
 	exit 1
 fi
